d9a63dde83c8878780c4fdca5318a5a7423d8a73
ayazali <ayazali.xwave@gmail.com>
2025-02-20 21:46:24 +0000

    turms

--- src/Components/Turms/Turms.jsx
+++ src/Components/Turms/Turms.jsx
@@ -23,14 +23,14 @@ const Turms = () => {
         <div className="col-md-10">
           <h4 className="mb-3">Terms of Service</h4>
           <p>
-            These Terms of Service apply to all purchases and use of NBK EXCLUSIVE's website.
+            These Terms of Service apply to all purchases and use of  website.
           </p>
           <ul>
             <li>
               <strong>Acceptance of Terms:</strong> By using our website and services, you agree to comply with these terms. If you do not agree with any part of the terms, you must not use our services.
             </li>
             <li>
-              <strong>Website Usage:</strong> The NBK EXCLUSIVE website and all its content (text, images, logos, etc.) are for personal, non-commercial use only. Unauthorized reproduction, duplication, or misuse of website content is strictly prohibited.
+              <strong>Website Usage:</strong> The  website and all its content (text, images, logos, etc.) are for personal, non-commercial use only. Unauthorized reproduction, duplication, or misuse of website content is strictly prohibited.
             </li>
             <li>
               <strong>Pricing and Product Availability:</strong> All prices are displayed in USD. Prices and product availability are subject to change without notice. While we strive to ensure accuracy, NBK EXCLUSIVE is not responsible for pricing errors or discontinued items.
@@ -39,10 +39,10 @@ const Turms = () => {
               <strong>Order Acceptance:</strong> We reserve the right to refuse or cancel any order at our discretion. If an order is canceled after payment, we will issue a full refund.
             </li>
             <li>
-              <strong>Limitation of Liability:</strong> NBK EXCLUSIVE will not be liable for any indirect, incidental, or consequential damages arising from the use of our products or website. Our liability is limited to the amount you paid for the product.
+              <strong>Limitation of Liability:</strong>   will not be liable for any indirect, incidental, or consequential damages arising from the use of our products or website. Our liability is limited to the amount you paid for the product.
             </li>
             <li>
-              <strong>User Accounts:</strong> When creating an account on our website, you are responsible for maintaining the confidentiality of your login information. NBK EXCLUSIVE is not liable for unauthorized access to your account due to negligence.
+              <strong>User Accounts:</strong> When creating an account on our website, you are responsible for maintaining the confidentiality of your login information. is not liable for unauthorized access to your account due to negligence.
             </li>
             <li>
               <strong>Governing Law:</strong> These terms and conditions are governed by the laws of Texas, without regard to conflict of laws principles. Disputes will be resolved through binding arbitration in Texas.
